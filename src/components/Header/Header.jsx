33396da6bce9ff44da7ab6d54b6a38dacb47dfdb
sefu <zephaniahsefuoduori@gmail.com>
2023-02-10 22:32:55 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="logo-name">ZEPH</div>
+      <div className="logo-name">STK-io</div>
       {menuOpened === false && mobile === true ? (
         <div
           style={{
